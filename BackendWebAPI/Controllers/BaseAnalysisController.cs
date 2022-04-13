9a461e742c64ba896f0eb674eb54e51d01071dcd
KleprlikJan <kleprlikjan@gmail.com>
2022-04-13 13:41:28 +0200

    Add BaseAnal1 impl

--- BackendWebAPI/Controllers/BaseAnalysisController.cs
+++ BackendWebAPI/Controllers/BaseAnalysisController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Shared.Models;
 
 namespace BackendWebAPI.Controllers
@@ -6,41 +7,94 @@ namespace BackendWebAPI.Controllers
     [Route("api/[controller]")]
     public class BaseAnalysisController : ControllerBase
     {
+        private CommonDbContext _context;
+
+        public BaseAnalysisController(CommonDbContext context)
+        {
+            _context = context;
+        }
 
         [HttpPost("[action]")]
         public async Task<ActionResult<List<AvgPassRateResponse>>> GetAvgPassRate([FromBody] DateTime from, [FromBody] DateTime to)
         {
-            //TODO query analisis analysis
-            //LIB.GETAVG(from, to)
+            List<IGrouping<string, Head>> prodGroups = await _context.Heads
+                .Where(x => x.TimeStamp >= from)
+                .Where(x => x.TimeStamp <= to)
+                .GroupBy(x => x.Product_SFIdString)
+                //.DistinctBy(x => x.Product_SFIdString)
+                .ToListAsync();
 
-            var result = new List<AvgPassRateResponse> {
-                new AvgPassRateResponse
+
+
+            var result = new List<AvgPassRateResponse>();
+            foreach (var prodGroup in prodGroups)
+            {
+                int passCounter = 0;
+                int totalCounter = 0;
+                foreach (var product in prodGroup)
                 {
-                    AvgPassRate = 0.8,
-                    Code = "N/A",
-                    Family = "IL4",
-                    HwVersion = "N/A",
-                    Name = "IL4 PG24A",
-                    SFCode = "PG24A",
-                    SFIdString = "PG24ANV21510A00",
-                    SFSN = "21510A00",
-                    SN = "N/A"
-                },
-                new AvgPassRateResponse
+                    totalCounter++;
+
+                    if (product.Result_Value == ResultTestEnum.PASS)
+                        passCounter++;
+                }
+                var exProduct = prodGroup.First();
+
+                var prodResult = new AvgPassRateResponse()
                 {
-                    AvgPassRate = 0.2,
-                    Code = "N/A",
-                    Family = "IL5",
-                    HwVersion = "N/A",
-                    Name = "IL5 PG24B",
-                    SFCode = "PG24B",
-                    SFIdString = "PG24BNV21510A00",
-                    SFSN = "21510B00",
-                    SN = "N/A"
-                },
-            };
+                    AvgPassRate = passCounter / totalCounter,
+                    Code = exProduct.Product_Code,
+                    Family = exProduct.Product_Family,
+                    HwVersion = exProduct.Product_HwVersion,
+                    Name = exProduct.Product_Name,
+                    SFCode = exProduct.Product_SFCode,
+                    SFIdString = exProduct.Product_SFIdString,
+                    SFSN = exProduct.Product_SFSN,
+                    SN = exProduct.Product_SN
+                };
+
+                result.Add(prodResult);
+            }
+
+
+
+
+            //var result = new List<AvgPassRateResponse> {
+            //    new AvgPassRateResponse
+            //    {
+            //        AvgPassRate = 0.8,
+            //        Code = "N/A",
+            //        Family = "IL4",
+            //        HwVersion = "N/A",
+            //        Name = "IL4 PG24A",
+            //        SFCode = "PG24A",
+            //        SFIdString = "PG24ANV21510A00",
+            //        SFSN = "21510A00",
+            //        SN = "N/A"
+            //    },
+            //    new AvgPassRateResponse
+            //    {
+            //        AvgPassRate = 0.2,
+            //        Code = "N/A",
+            //        Family = "IL5",
+            //        HwVersion = "N/A",
+            //        Name = "IL5 PG24B",
+            //        SFCode = "PG24B",
+            //        SFIdString = "PG24BNV21510A00",
+            //        SFSN = "21510B00",
+            //        SN = "N/A"
+            //    },
+            //};
 
             return Ok(result);
+        }
+
+        [HttpPost("[action]")]
+        public async Task<IActionResult> GetAvgTestLength([FromBody] DateTime from, [FromBody] DateTime to)
+        {
+
+
+            return Ok();
         }
     }
 }
